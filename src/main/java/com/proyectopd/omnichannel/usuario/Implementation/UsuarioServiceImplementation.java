f410a6aac9e85586b188fc0081f6f2382ecef3ef
camunozv <93873199+camunozv@users.noreply.github.com>
2025-04-01 15:00:45 -0500

    Create and get user implemented

--- src/main/java/com/proyectopd/omnichannel/usuario/Implementation/UsuarioServiceImplementation.java
+++ src/main/java/com/proyectopd/omnichannel/usuario/Implementation/UsuarioServiceImplementation.java
@@ -1,19 +1,42 @@
 package com.proyectopd.omnichannel.usuario.Implementation;
 
 import com.proyectopd.omnichannel.usuario.Usuario;
+import com.proyectopd.omnichannel.usuario.UsuarioRepository;
 import com.proyectopd.omnichannel.usuario.UsuarioService;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class UsuarioServiceImplementation implements UsuarioService {
 
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImplementation(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
 
     @Override
     public Usuario getUsuarioById(Long cedula) {
-        return null;
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(cedula);
+        Usuario usuario = null;
+        if (usuarioOptional.isPresent()) {
+            usuario = usuarioOptional.get();
+        }
+        return usuario;
     }
 
     @Override
     public boolean createUsuario(Usuario usuario) {
-        return false;
+
+        boolean created;
+        try {
+            usuarioRepository.save(usuario);
+            created = true;
+        } catch (Exception e) {
+            created = false;
+        }
+        return created;
     }
 
     @Override
--- src/main/java/com/proyectopd/omnichannel/usuario/UsuarioService.java
+++ src/main/java/com/proyectopd/omnichannel/usuario/UsuarioService.java
@@ -3,10 +3,12 @@ package com.proyectopd.omnichannel.usuario;
 import java.util.List;
 
 public interface UsuarioService {
-
+    // Implemented
     Usuario getUsuarioById(Long cedula);
-//    List<Usuario> getAllUsuarios();
     boolean createUsuario(Usuario usuario);
+
+    // Not implemented
     boolean updateUsuario(Long cedula, Usuario usuario);
     boolean deleteUsuario(Long cedula);
+    //    List<Usuario> getAllUsuarios();
 }
