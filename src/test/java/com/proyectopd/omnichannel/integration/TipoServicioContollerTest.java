d8710d910f89d593a947f75f50f80ab4b07a2200
camunozv <93873199+camunozv@users.noreply.github.com>
2025-05-05 19:00:08 -0500

    Added test on create tipoServicio controller

--- src/test/java/com/proyectopd/omnichannel/integration/TipoServicioContollerTest.java
+++ src/test/java/com/proyectopd/omnichannel/integration/TipoServicioContollerTest.java
@@ -1,4 +1,35 @@
 package com.proyectopd.omnichannel.integration;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)
 public class TipoServicioContollerTest {
+
+    String BASE_URL = "/tipoServicio";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testCreateTipoServicio() throws Exception {
+
+        String requestBody = "{\"nombreServicio\":\"Alcantarillado\"}";
+
+        mockMvc.perform(post(BASE_URL).contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)).andExpect(status().isOk())
+                .andExpect(content().string("{\"nombreServicio\":\"Alcantarillado\",\"empresa\":null}"));
+
+    }
 }
