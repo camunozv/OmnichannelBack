ed54a8b8e33a60c199d9736a2bba23f355860aae
camunozv <93873199+camunozv@users.noreply.github.com>
2025-05-26 09:32:20 -0500

    Added 5 tests on QuejaServiceImpl

--- src/test/java/com/proyectopd/omnichannel/service/QuejaServiceTests.java
+++ src/test/java/com/proyectopd/omnichannel/service/QuejaServiceTests.java
@@ -1,13 +1,25 @@
 package com.proyectopd.omnichannel.service;
 
+import com.proyectopd.omnichannel.models.Empresa;
+import com.proyectopd.omnichannel.models.Profesional;
+import com.proyectopd.omnichannel.models.Queja;
+import com.proyectopd.omnichannel.models.Respuesta;
 import com.proyectopd.omnichannel.repositories.NotificacionRepository;
 import com.proyectopd.omnichannel.repositories.ProfesionalRepository;
 import com.proyectopd.omnichannel.repositories.QuejaRepository;
 import com.proyectopd.omnichannel.services.Implementation.QuejaServiceImplementation;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class QuejaServiceTests {
@@ -22,57 +34,120 @@ public class QuejaServiceTests {
     @InjectMocks
     private QuejaServiceImplementation quejaServiceImplementation;
 
+    @Test
+    public void testGetAllQuejasEmpresa() {
+        ArrayList<Queja> quejasEmpresa = new ArrayList<>();
+        Empresa empresa = new Empresa();
+        empresa.setNombreEmpresa("Empresa de prueba");
+        for(int i = 0; i < 10; i++) {
+            Queja queja = new Queja();
+            queja.setEmpresa(empresa);
+            queja.setIdQueja(i);
+        }
+        when(quejaRepository.findQuejasByEmpresa_NombreEmpresaEquals(empresa.getNombreEmpresa())).thenReturn(quejasEmpresa);
 
-    /*
-    @Override
-    public List<Queja> getAllQuejasEmpresa(String nombreEmpresa) {
-        return quejaRepository.findQuejasByEmpresa_NombreEmpresaEquals(nombreEmpresa);
-    }
+        ArrayList<Queja> quejasEmpresaTest = (ArrayList<Queja>) quejaServiceImplementation.getAllQuejasEmpresa("Empresa de prueba");
 
-
-    @Override
-    public Queja getQuejaById(Integer idQueja) {
-        return quejaRepository.getQuejaByIdQueja(idQueja);
-    }
-
-    @Override
-    public List<Queja> getQuejasByEstado(String estado) {
-        return quejaRepository.findQuejasByEstado(estado);
-    }
-
-
-    @Override
-    public boolean createQueja(Queja queja) {
-        boolean created;
-
-        try {
-            quejaRepository.save(queja);
-            created = true;
-        } catch (Exception e) {
-            created = false;
+        for(int i = 0; i < quejasEmpresaTest.size(); i++) {
+            assertEquals(quejasEmpresa.get(i), quejasEmpresaTest.get(i));
         }
 
-        return created;
+        verify(quejaRepository, times(1)).findQuejasByEmpresa_NombreEmpresaEquals("Empresa de prueba");
     }
 
+    @Test
+    public void testGetQuejaById() {
+        Queja queja = new Queja();
+        queja.setIdQueja(1);
+        queja.setEstado("RESPONDIDA");
+        when(quejaRepository.getQuejaByIdQueja(1)).thenReturn(queja);
 
-    @Override
-    public boolean answerQueja(Respuesta respuesta, Integer idQueja) {
+        Queja quejaTest = quejaServiceImplementation.getQuejaById(1);
 
-        boolean answered = false;
-        Optional<Queja> quejaOptional = quejaRepository.findById(idQueja);
+        assertEquals(queja, quejaTest);
+        verify(quejaRepository, times(1)).getQuejaByIdQueja(1);
+    }
 
-        if (quejaOptional.isPresent()) {
-            Queja queja = quejaOptional.get();
-            queja.setRespuesta(respuesta);
+    @Test
+    public void testGetQuejasByEstado() {
+        ArrayList<Queja> quejas = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            Queja queja = new Queja();
+            queja.setIdQueja(i);
             queja.setEstado("RESPONDIDA");
-            queja.setTiempoMinimoRespuesta(LocalDate.MAX);
-            quejaRepository.save(queja);
-            answered = true;
+            quejas.add(queja);
         }
 
-        return answered;
+        when(quejaRepository.findQuejasByEstado("RESPONDIDA")).thenReturn(quejas);
+
+        ArrayList<Queja> quejasTest = (ArrayList<Queja>) quejaServiceImplementation.getQuejasByEstado("RESPONDIDA");
+
+        for(int i = 0; i < quejasTest.size(); i++) {
+            assertEquals(quejas.get(i), quejasTest.get(i));
+        }
+
+        verify(quejaRepository, times(1)).findQuejasByEstado("RESPONDIDA");
     }
+
+    @Test
+    public void testCreateQueja () {
+        Queja queja = new Queja();
+        queja.setIdQueja(1);
+        queja.setEstado("RESPONDIDA");
+        when(quejaRepository.save(queja)).thenReturn(queja);
+
+        boolean created = quejaServiceImplementation.createQueja(queja);
+
+        assertEquals(true, created);
+
+        verify(quejaRepository, times(1)).save(queja);
+    }
+
+    @Test
+    public void testAnswerQueja() {
+        int idQueja = 1;
+        Queja queja = new Queja();
+        queja.setIdQueja(idQueja);
+
+        Respuesta respuesta = new Respuesta();
+        respuesta.setIdRespuesta(1);
+        respuesta.setQueja(queja);
+        respuesta.setTextoRespuesta("Hola");
+
+        when(quejaRepository.findById(idQueja)).thenReturn(Optional.of(queja));
+        when(quejaRepository.save(queja)).thenReturn(queja);
+
+        boolean answered = quejaServiceImplementation.answerQueja(respuesta, idQueja);
+
+        assertEquals(true, answered);
+
+        verify(quejaRepository, times(1)).findById(idQueja);
+        verify(quejaRepository, times(1)).save(queja);
+    }
+
+    @Test
+    public void testAssignProfesionalAllQuejasAssigned() {
+        ArrayList<Queja> quejasVencidas = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            Queja queja = new Queja();
+            queja.setIdQueja(i);
+            queja.setTiempoMinimoRespuesta(LocalDate.of(2001, 1, 1));
+            queja.setEstado("SIN RESPONDER");
+            quejasVencidas.add(queja);
+        }
+
+        ArrayList<Profesional> profesionalesLibres = new ArrayList<>();
+        for(int i = 0; i < 5; i++) {
+            Profesional profesional = new Profesional();
+            profesional.setIdProfesional(i);
+            profesional.setCantidadQuejasEncargadas(1);
+            profesionalesLibres.add(profesional);
+        }
+
+        when(quejaRepository.findQuejasByTiempoMinimoRespuestaIsLessThan(LocalDate.now())).thenReturn(quejasVencidas);
+        when(profesionalRepository.findProfesionalsByCantidadQuejasEncargadasIsLessThan(3)).thenReturn(profesionalesLibres);
+    }
+    /*
 
     @Override
     public boolean assignProfesional() {
