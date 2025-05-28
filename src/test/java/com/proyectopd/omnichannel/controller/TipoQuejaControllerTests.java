f52b85b87ba1abe67a5dff7ce35208fbb9fb688a
camunozv <93873199+camunozv@users.noreply.github.com>
2025-05-27 20:24:41 -0500

    Finished unit tests on TipoQuejaController

--- src/test/java/com/proyectopd/omnichannel/controller/TipoQuejaControllerTests.java
+++ src/test/java/com/proyectopd/omnichannel/controller/TipoQuejaControllerTests.java
@@ -1,10 +1,15 @@
 package com.proyectopd.omnichannel.controller;
 
 import com.proyectopd.omnichannel.controllers.NotificacionController;
+import com.proyectopd.omnichannel.controllers.TipoQuejaController;
+import com.proyectopd.omnichannel.models.TipoQueja;
 import com.proyectopd.omnichannel.services.TipoQuejaService;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(NotificacionController.class)
+@WebMvcTest(TipoQuejaController.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class TipoQuejaControllerTests {
 
@@ -24,4 +29,46 @@ public class TipoQuejaControllerTests {
     private MockMvc mockMvc;
     @MockitoBean
     private TipoQuejaService tipoQuejaService;
+
+    @Test
+    public void testCreateTipoQueja() throws Exception {
+        String requestBody = "{"
+                + "\"tipoQueja\": \"Retraso\""
+                + ",\"dias\": \"1\""
+                + "}";
+
+        when(tipoQuejaService.createTipoQueja(ArgumentMatchers.any(TipoQueja.class))).thenReturn(true);
+
+        mockMvc.perform(post(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.tipoQueja").value("Retraso"));
+    }
+
+    @Test
+    public void testGetTipoQueja() throws Exception {
+        TipoQueja tipoQueja = new TipoQueja();
+        tipoQueja.setTipoQueja("Retraso");
+        tipoQueja.setDias(1);
+
+        when(tipoQuejaService.getTipoQuejaById("Retraso")).thenReturn(tipoQueja);
+
+        mockMvc.perform(get(BASE_URL)
+                        .param("nombreTipoQueja", "Retraso"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.tipoQueja").value("Retraso"))
+                .andExpect(jsonPath("$.dias").value(1));
+    }
+
+    @Test
+    public void testDeleteTipoQueja() throws Exception {
+        when(tipoQuejaService.deleteTipoQueja("Retraso")).thenReturn(true);
+        mockMvc.perform(delete(BASE_URL)
+                        .param("nombreTipoQueja", "Retraso"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(true));
+    }
+
 }
